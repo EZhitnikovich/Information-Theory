9ac6519ac7453de96821fd9a5e1b9624e32ceb40
EZhitnikovich <ezhitnikovich@gmail.com>
2020-12-22 00:06:05 +0300

    add lab3

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sourceOriginal = new System.Windows.Forms.TextBox();
+            this.outCipher = new System.Windows.Forms.TextBox();
+            this.sourceCipher = new System.Windows.Forms.TextBox();
+            this.outOriginal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(1000, 166);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(413, 132);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1000, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(413, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(7, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(482, 73);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Зашифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(495, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(499, 73);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Расшифровать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(1000, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(413, 49);
+            this.textBox1.TabIndex = 5;
+            // 
+            // sourceOriginal
+            // 
+            this.sourceOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sourceOriginal.Location = new System.Drawing.Point(7, 54);
+            this.sourceOriginal.Multiline = true;
+            this.sourceOriginal.Name = "sourceOriginal";
+            this.sourceOriginal.Size = new System.Drawing.Size(482, 244);
+            this.sourceOriginal.TabIndex = 6;
+            // 
+            // outCipher
+            // 
+            this.outCipher.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.outCipher.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outCipher.Location = new System.Drawing.Point(12, 425);
+            this.outCipher.Multiline = true;
+            this.outCipher.Name = "outCipher";
+            this.outCipher.Size = new System.Drawing.Size(477, 279);
+            this.outCipher.TabIndex = 7;
+            // 
+            // sourceCipher
+            // 
+            this.sourceCipher.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sourceCipher.Location = new System.Drawing.Point(495, 54);
+            this.sourceCipher.Multiline = true;
+            this.sourceCipher.Name = "sourceCipher";
+            this.sourceCipher.Size = new System.Drawing.Size(499, 244);
+            this.sourceCipher.TabIndex = 8;
+            // 
+            // outOriginal
+            // 
+            this.outOriginal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.outOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outOriginal.Location = new System.Drawing.Point(495, 425);
+            this.outOriginal.Multiline = true;
+            this.outOriginal.Name = "outOriginal";
+            this.outOriginal.Size = new System.Drawing.Size(499, 279);
+            this.outOriginal.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(488, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(441, 42);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Расшифрованный текст";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(0, 380);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(420, 42);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Зашифрованный текст";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(0, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(298, 42);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Исходный текст";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(488, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(420, 42);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Зашифрованный текст";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(1000, 425);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(413, 279);
+            this.textBox2.TabIndex = 17;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(1000, 304);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(412, 53);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Вычислить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(993, 380);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 42);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Значения регистра:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1425, 716);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.outOriginal);
+            this.Controls.Add(this.sourceCipher);
+            this.Controls.Add(this.outCipher);
+            this.Controls.Add(this.sourceOriginal);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGrid);
+            this.MaximumSize = new System.Drawing.Size(1441, 755);
+            this.MinimumSize = new System.Drawing.Size(1441, 755);
+            this.Name = "Form1";
+            this.Text = "Lab3";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox sourceOriginal;
+        private System.Windows.Forms.TextBox outCipher;
+        private System.Windows.Forms.TextBox sourceCipher;
+        private System.Windows.Forms.TextBox outOriginal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab3/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Math;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        byte Capacity;
+        public Form1()
+        {
+            InitializeComponent();
+            Capacity = 8;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int value))
+            {
+                if (value < 9 && value > 0)
+                {
+                    Capacity = Convert.ToByte(textBox1.Text);
+                    dataGrid.ColumnCount = Convert.ToInt16(textBox1.Text);
+                    dataGrid.RowCount = 3;
+                    for (int i = 0; i < dataGrid.ColumnCount; i++)
+                    {
+                        dataGrid.Columns[i].Name = Convert.ToString(i + 1);
+                        dataGrid.Columns[i].Width = 40;
+                        dataGrid.Rows[0].Cells[i].Value = i % 2;
+                        dataGrid.Rows[1].Cells[i].Value = 0;
+                        if(i == 0 || i == dataGrid.ColumnCount - 1)
+                            dataGrid.Rows[2].Cells[i].Value = 1;
+                        else
+                            dataGrid.Rows[2].Cells[i].Value = 0;
+                    }
+                    dataGrid.RowHeadersWidth = 170;
+                    dataGrid.Rows[0].HeaderCell.Value = "Изначальное состояние";
+                    dataGrid.Rows[1].HeaderCell.Value = "Промежуточные";
+                    dataGrid.Rows[2].HeaderCell.Value = "Отводы";
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int value))
+            {
+
+                textBox2.Clear();
+
+
+                int loop = ((int)Pow(2, value) - 1) * 2;
+
+                int g = 1;
+                for (int i = 0; i < Capacity; i++)
+                    dataGrid.Rows[1].Cells[i].Value = dataGrid.Rows[0].Cells[i].Value;
+                while (loop != 0)
+                {
+                    textBox2.Text += $"{g} - ";
+                    for (int i = 0; i < value; i++)
+                    {
+                        textBox2.Text += dataGrid.Rows[1].Cells[i].Value;
+                    }
+
+                    textBox2.Text += "\r\n";
+                    loop--;
+                    g++;
+                    GetNewKey();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Capacity; i++)
+                dataGrid.Rows[1].Cells[i].Value = dataGrid.Rows[0].Cells[i].Value;
+
+            string source = sourceOriginal.Text;
+
+            outCipher.Text = TransformText(source);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Capacity; i++)
+                dataGrid.Rows[1].Cells[i].Value = dataGrid.Rows[0].Cells[i].Value;
+
+            string source = sourceCipher.Text;
+
+            outOriginal.Text = TransformText(source);
+        }
+
+        private string TransformText(string source)
+        {
+            string result = null;
+
+            int c = 0;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                string key = GetKey();
+                GetNewKey();
+                c++;
+                result += Convert.ToChar(source[i] ^ BinToByte(key));
+            }
+
+            return result;
+        }
+
+        string GetKey()
+        {
+            string key_str = "";
+            for (int i = 0; i < Capacity; i++)
+                key_str += dataGrid.Rows[1].Cells[i].Value;
+            return key_str;
+        }
+
+        void GetNewKey()
+        {
+
+            int temp = 1;
+            List<int> next = new List<int>();
+
+            for (int i = 0; i < Capacity; i++)
+            {
+                if (Convert.ToInt16(dataGrid.Rows[2].Cells[i].Value) == 1)
+                {
+                    temp = i;
+                    next.Add(Convert.ToInt16(dataGrid.Rows[1].Cells[i].Value));
+                    break;
+                }
+            }
+
+            for (int i = temp + 1; i < Capacity; i++)
+            {
+                if (Convert.ToInt16(dataGrid.Rows[2].Cells[i].Value) == 1)
+                {
+                    next[0] = next[0] ^ Convert.ToInt16(dataGrid.Rows[1].Cells[i].Value) ^ 1;
+                }
+            }
+
+            for (int i = 0; i < Capacity - 1; i++)
+            {
+                next.Add(Convert.ToInt16(dataGrid.Rows[1].Cells[i].Value));
+            }
+
+            for (int i = 0; i < Capacity; i++)
+                dataGrid.Rows[1].Cells[i].Value = next[i];
+        }
+
+        byte BinToByte(string binary)
+        {
+            byte result = 0, val = 0;
+            for (int i = 0; i < Capacity; i++)
+            {
+                if (binary[i] == '0') val = 0;
+                else val = 1;
+                result += Convert.ToByte(Math.Truncate(val * Math.Exp(Math.Log10(2) * (Capacity - i))));
+            }
+            return result;
+        }
+    }
+}
